0897c3ca0f296d365c0a4afdb615eb59226066f1
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2023-05-10 16:48:09 -0300

    added delete button and function

--- src/components/alunos/Alunos.jsx
+++ src/components/alunos/Alunos.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './aluno.css'
+import DeleteIcon from '@mui/icons-material/Delete';
+import axios from 'axios';
 
 const EndPointBackEndSpring = 'http://localhost:8080/aluno'
 const EndpointSpringBase = 'http://localhost:8080'
@@ -7,6 +9,9 @@ const EndpointSpringBase = 'http://localhost:8080'
 const Alunos = () => {
 
     const [alunosData, setAlunosData] = useState([]);
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [curso, setCurso] = useState("")
 
     useEffect(() => {
         async function fetchData() {
@@ -17,12 +22,20 @@ const Alunos = () => {
 
         }
         fetchData()
-    }, []);
+    }, [])
 
 
-    const [nome, setNome] = useState("");
-    const [email, setEmail] = useState("");
-    const [curso, setCurso] = useState("");
+
+    const serviceDelete = async (id) => {
+        console.log(id)
+        try {
+          await axios.delete(`${EndPointBackEndSpring}/${id}`);
+          // eslint-disable-next-line no-restricted-globals
+          location.reload();
+        } catch (error) {
+            console.error("Erro ao excluir o item: ", error)
+        }
+      };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -33,7 +46,7 @@ const Alunos = () => {
         }
         const response = await fetch(EndPointBackEndSpring, {
             method: 'POST',
-            headers:{
+            headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(addAluno)
@@ -45,61 +58,74 @@ const Alunos = () => {
         setNome('')
         setEmail('')
         setCurso('')
-    };
+    }
 
     return (
         <div>
 
             <h1>Alunos</h1>
-            <table border='1px'>
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <th>Curso</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {alunosData.map((aluno) => (
-                        <tr key={aluno.id}>
-                            <td className='nome'>{aluno.nome}</td>
-                            <td className='email'>{aluno.email}</td>
-                            <td>{aluno.curso}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
 
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Nome:
-                    <input
-                        type="text"
-                        value={nome}
-                        onChange={(event) => setNome(event.target.value)}
-                    />
-                </label>
-                <br />
-                <label>
-                    Email:
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                    />
-                </label>
-                <br />
-                <label>
-                    Curso:
-                    <input
-                        type="text"
-                        value={curso}
-                        onChange={(event) => setCurso(event.target.value)}
-                    />
-                </label>
-                <br />
-                <button type="submit">Enviar</button>
-            </form>
+            <div className='container'>
+
+                <table cellSpacing={5}>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Email</th>
+                            <th>Curso</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {alunosData.map((aluno) => (
+                            <tr key={aluno.id}>
+                                {console.log(aluno)}
+                                <td className='nome'>{aluno.nome}</td>
+                                <td className='email'>{aluno.email}</td>
+                                <td>{aluno.curso}</td>
+                                <td><button onClick={() => serviceDelete(aluno.id)} className='error'><DeleteIcon></DeleteIcon></button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Nome:
+                        <input
+                            type="text"
+                            value={nome}
+                            onChange={(event) => setNome(event.target.value)}
+                            required
+                            placeholder='Digite o nome do aluno'
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        Email:
+                        <input
+                            placeholder='Digite o email do aluno'
+                            required
+                            type="email"
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        Curso:
+                        <input
+                            placeholder='Digite o curso do aluno'
+                            type="text"
+                            value={curso}
+                            onChange={(event) => setCurso(event.target.value)}
+                            required
+                        />
+                    </label>
+                    <br />
+                    <button className='enviar' type="submit">Enviar</button>
+                </form>
+            </div>
         </div>
     )
 }
