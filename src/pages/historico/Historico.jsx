376a3ae91b540220267ead978916830b7ed2846e
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2023-05-28 02:50:24 -0300

    removed unused import

--- src/pages/historico/Historico.jsx
+++ src/pages/historico/Historico.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { EndpointSpringBase } from '../../constants/conts'
 import './historico.module.css'
 
